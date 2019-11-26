8bcaef2e50c571301a875bc75569d2ce35c45be3
Vova <vovaleonov1021@gmail.com>
2019-11-26 14:07:05 +0200

    add cycles to my code

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
-let money = prompt("Ваш бюджет на месяц");
+let money = +prompt("Ваш бюджет на месяц");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
-let spendMoney = prompt("Введите обязательную статью расходов в этом месяце");
-let howMuch = prompt("Во сколько обойдется?");
 
 let appData = {
     budget: money,
-    timeData: time,
-      
-    expenses: { 
-    [spendMoney] : howMuch
-    },
-
-    optionalExpenses: {
-
-    },
-
+    timeData: time,      
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     savings: false,
+};
+
+    for (let i=0; i<2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+        let b = prompt("Во сколько обойдется?", '');
+
+    // let i = 0;
+    // while (i<2) {
+    //     let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    //     let b = prompt("Во сколько обойдется?", '');    
+    // i++;
+
+    // let i = 0;
+    // do {
+    //     let a = prompt("Введите обязательную статью расходов в этом месяце", '');
+    //     let b = prompt("Во сколько обойдется?", '');
+    //     i++;
+    //  while (i<2);
+
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+    && a != '' && b != '' && a.length < 50 ) {
+        console.log ("done");
+        appData.expenses[a]= b;
+    } else {
+
+    }
 }
 
 
+appData.moneyPerDay = appData.budget / 30;
 
-alert("ваш бюджет на один день составит: " + money/30)
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Прожиточный минимум");
+} else if (appData.moneyPerDay < 2000) {
+    console.log("Жить можно");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Шикуем");
+} else {
+        console.log("Error");
+    }
